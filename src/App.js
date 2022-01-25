4bca253dd1d72936b5a73225f9220aaf6add6b33
“Gabriel <gecarrizo@gmail.com>
2022-01-25 13:13:42 -0300

    Clase 6 - React - Desafio 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className='App'>
+          <Navbar/>
+          <ItemListContainer greeting="Catálogo de productos de la empresa."/>
+      </div>
+    );
 }
 
 export default App;
